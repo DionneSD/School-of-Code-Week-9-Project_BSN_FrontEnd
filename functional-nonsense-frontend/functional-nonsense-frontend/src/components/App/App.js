64245dd33e56de63ed2c6cbe09a4f4c4b4f0d0f0
CTHall05 <charlestaylorhall05@gmail.com>
2022-11-22 11:55:18 +0000

    Taylor first commit for frontend

---
+++ functional-nonsense-frontend/functional-nonsense-frontend/src/components/App/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+import LandingPage from '../LandingPage/landingPage.js';
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage></LandingPage>
+    </div>
+  );
+}
+
+export default App;
---
+++ functional-nonsense-frontend/functional-nonsense-frontend/src/components/LandingPage/landingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function LandingPage() {
+  return (
+    <div>
+      <h1>App Name!</h1>
+      <div className="product-div">
+        Connect with Bootcampers
+      </div>
+    </div>
+  )
+}
