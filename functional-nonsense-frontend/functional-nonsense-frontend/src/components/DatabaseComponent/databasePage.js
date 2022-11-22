830ee8050d4a02872787116e57531fc3b3cabee6
CTHall05 <charlestaylorhall05@gmail.com>
2022-11-22 15:59:04 +0000

    Taylor commit changing something!

--- functional-nonsense-frontend/functional-nonsense-frontend/src/components/DatabaseComponent/databasePage.js
+++ functional-nonsense-frontend/functional-nonsense-frontend/src/components/DatabaseComponent/databasePage.js
@@ -2,6 +2,30 @@ import React from 'react'
 
 export default function DatabasePage() {
   return (
-    <div>DatabasePage</div>
+    <div>
+      <h1>DatabasePage</h1>
+      <div className="databaseResults">
+        <ol>
+          <li>Id</li>
+          <li>Name</li>
+          <li>Learning Style</li>
+          <li>Myers Briggs Personality</li>
+          <li>Religion</li>
+          <li>Cultural Background</li>
+          <li>Gender</li>
+          <li>Sexuality</li>
+          <li>Hobby</li>
+          <li>Twitter</li>
+          <li>Linked-in</li>
+          <li>Facebook</li>
+          <li>Slack</li>
+          <li>Email</li>
+        </ol>
+      </div>
+      <div className="nextStep-buttons">
+        <button>Join Community</button>
+        
+      </div>
+    </div>
   )
 }
