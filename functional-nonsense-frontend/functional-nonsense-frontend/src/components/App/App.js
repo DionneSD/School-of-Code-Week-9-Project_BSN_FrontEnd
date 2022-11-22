b957dc151e1a2e686d65caab33e6244f9df79740
DionneSD <111356485+DionneSD@users.noreply.github.com>
2022-11-22 15:54:43 +0000

    input page done

--- functional-nonsense-frontend/functional-nonsense-frontend/src/components/App/App.js
+++ functional-nonsense-frontend/functional-nonsense-frontend/src/components/App/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
-
 import LandingPage from '../LandingPage/landingPage.js';
+import InputForm from '../InputForm/inputForm';
 
 function App() {
   return (
     <div className="App">
       <LandingPage></LandingPage>
+      <InputForm></InputForm>
     </div>
   );
 }
