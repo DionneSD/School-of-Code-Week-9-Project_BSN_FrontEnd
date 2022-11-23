830143362f6290c699e2504ba2dd0dc2c5c90a9a
Seb French <Seb@Sebs-MacBook-Pro.local>
2022-11-23 13:43:33 +0000

    cleanup

--- functional-nonsense-frontend/src/components/App/App.js
+++ functional-nonsense-frontend/src/components/App/App.js
@@ -1,16 +1,22 @@
-
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import NavBar from '../NavBar/navBar';
 import LandingPage from '../Pages/LandingPage/landingPage';
-import DatabasePage from '../Pages/DatabasePage/databasePage';
 import InputPage from '../Pages/InputPage/inputPage';
+import DatabasePage from '../Pages/DatabasePage/databasePage';
 
 function App() {
   return (
     <div className="App">
-      <LandingPage></LandingPage>
-      <DatabasePage></DatabasePage>
-      <InputPage></InputPage>
+    <Router>
+    <NavBar />
+    <Routes>
+        {/* ignore the following squiggly line */}
+        <Route exact path='/' exact element={<LandingPage/>} />
+        <Route path='/join' element={<InputPage/>} />
+        <Route path='/database' element={<DatabasePage/>} />
+    </Routes>
+    </Router>
     </div>
   );
 }
--- functional-nonsense-frontend/src/components/Pages/LandingPage/landingPage.js
+++ functional-nonsense-frontend/src/components/Pages/LandingPage/landingPage.js
@@ -12,10 +12,6 @@ export default function LandingPage() {
         <h2>about us will go here</h2>
         <p>text here about the website</p>
       </div>
-      <div>
-        <button className="button" id="join-community">join community</button>
-        <button className="button" id="access-database">access database</button>
-      </div>
     </div>
   )
 };
