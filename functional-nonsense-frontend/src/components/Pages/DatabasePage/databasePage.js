f36624d6d421597a3db034d00df1599f78f1653d
CTHall05 <charlestaylorhall05@gmail.com>
2022-11-24 11:40:58 +0000

    Taylor commit Css in Database Component Started, Editing Database Componenet

--- functional-nonsense-frontend/src/components/Pages/DatabasePage/databasePage.js
+++ functional-nonsense-frontend/src/components/Pages/DatabasePage/databasePage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
+import "./databasePage.css"
 
 export default function DatabasePage() {
 
@@ -15,61 +16,64 @@ useEffect(()=>{
 }, [])
 
 const [allContacts, setAllContacts] = useState([])
-useEffect(()=>{
-  const getAllContacts = async () => {
-    const response = await fetch ('http://localhost:3001/contacts',{method: "GET"});
-    const data = await response.json()
-    console.log(data.payload)
-    setAllContacts(data.payload)
-  }
-  getAllContacts();
-}, [])
+  useEffect(()=>{
+    const getAllContacts = async () => {
+      const response = await fetch ('http://localhost:3001/contacts',{method: "GET"});
+      const data = await response.json()
+      console.log(data.payload)
+      setAllContacts(data.payload)
+    }
+    getAllContacts();
+  }, [])
 
   return (
     <div>
-      <h1>bootcamper database:</h1>
-      <div className="databaseResults">
-      {allContacts.map((contact, i)=> {
-      return <li key={i}>Name: {contact.name}</li>
-      })}
-      {allContacts.map((contact, i)=> {
-      return <li key={i}>Twitter: {contact.twitter}</li>
-      })}
-      {allContacts.map((contact, i)=> {
-      return <li key={i}>Linked-in: {contact.linked_in}</li>
-      })}
-      {allContacts.map((contact, i)=> {
-      return <li key={i}>Facebook: {contact.facebook}</li>
-      })}
-      {allContacts.map((contact, i)=> {
-      return <li key={i}>Slack: {contact.slack}</li>
-      })}
-      {allContacts.map((contact, i)=> {
-      return <li key={i}>Email: {contact.email}</li>
-      })}
-      {allUsers.map((user, i)=> {
-      return <li key={i}>Learning Style: {user.learning_style}</li>
-      })}
-      {allUsers.map((user, i)=> {
-      return <li key={i}>Myers-Briggs Personality: {user.mb_personality}</li>
-      })}
-      {allUsers.map((user, i)=> {
-      return <li key={i}>Religion: {user.religion}</li>
-      })}
-      {allUsers.map((user, i)=> {
-      return <li key={i}>Cultural Background: {user.cultural_background}</li>
-      })}
-      {allUsers.map((user, i)=> {
-      return <li key={i}>Gender Identity: {user.gender_id}</li>
-      })}
-      {allUsers.map((user, i)=> {
-      return <li key={i}>Sexuality: {user.sexuality}</li>
-      })}
-      {allUsers.map((user, i)=> {
-      return <li key={i}>Hobby: {user.hobby}</li>
-      })}
-      </div>
-    
+      <h1 className='h1Bootcamper'>Bootcamper Database:</h1>
+        <div className="databaseResults">
+          <div className="dataContainer">
+            {allContacts.map((contact, i)=> {return <ol>
+              <li key={i}>Name: {contact.name}</li>
+              <li key={i}>Twitter: {contact.twitter}</li>
+            </ol>  
+            })}
+          </div>
+          <div className="dataContainer">
+            {allContacts.map((contact, i)=> {return <li key={i}>Twitter: {contact.twitter}</li>})}
+          </div>
+          <div className="dataContainer">
+            {allContacts.map((contact, i)=> {return <li key={i}>Linked-in: {contact.linked_in}</li>})}
+          </div>
+          <div className='dataContainer'>
+            {allContacts.map((contact, i)=> {return <li key={i}>Facebook: {contact.facebook}</li>})}
+          </div>
+          <div className="dataContainer">
+            {allContacts.map((contact, i)=> {return <li key={i}>Slack: {contact.slack}</li>})}
+          </div>
+          <div className="dataContainer">
+            {allContacts.map((contact, i)=> {return <li key={i}>Email: {contact.email}</li>})}
+          </div>
+          <div className="dataContainer">
+            {allUsers.map((user, i)=> {return <li key={i}>Learning Style: {user.learning_style}</li>})}
+          </div>
+          <div className="dataContainer">
+            {allUsers.map((user, i)=> {return <li key={i}>Myers-Briggs Personality: {user.mb_personality}</li>})}
+          </div>
+          <div className="dataContainer">
+            {allUsers.map((user, i)=> {return <li key={i}>Religion: {user.religion}</li>})}  
+          </div>
+          <div className="dataContainer">
+            {allUsers.map((user, i)=> {return <li key={i}>Cultural Background: {user.cultural_background}</li>})}
+          </div>
+          <div className="dataContainer">
+            {allUsers.map((user, i)=> {return <li key={i}>Gender Identity: {user.gender_id}</li>})}
+          </div>
+          <div className="dataContainer">
+            {allUsers.map((user, i)=> {return <li key={i}>Sexuality: {user.sexuality}</li>})}    
+          </div>
+          <div className="dataContainer">
+            {allUsers.map((user, i)=> {return <li key={i}>Hobby: {user.hobby}</li>})}
+          </div>
+      </div>  
     </div>
   )
 }
