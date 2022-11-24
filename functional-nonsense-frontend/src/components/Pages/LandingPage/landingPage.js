73d93eebf4e54fd77afc388dd35a60016a4fb214
Fliss317 <felicity.cullen@mail.com>
2022-11-24 11:39:57 +0000

    landing page text and tidy done

--- functional-nonsense-frontend/src/components/Pages/LandingPage/landingPage.js
+++ functional-nonsense-frontend/src/components/Pages/LandingPage/landingPage.js
@@ -6,7 +6,7 @@ export default function LandingPage() {
     <div>
       <h1>bootcamper support network</h1>
       <figure className="image">
-        <img id="image" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg" alt="insert here"/>
+        <img id="image" src="https://media.istockphoto.com/id/1285126883/photo/group-multiracial-friends-having-fun-outdoor-happy-mixed-race-people-taking-selfie-together.jpg?s=612x612&w=0&k=20&c=jZbQHkSHJY45o6UKlr7nt1aJ-sMIMHUNCsVYc6syY3U=" alt="a group of people"/>
       </figure>
       <div className="about-box">
         <h2>about us</h2>
@@ -14,7 +14,8 @@ export default function LandingPage() {
         <p>this website was created to enable tech bootcampers around the world to connect with each other and help each other learn. it has purposefully been designed to be as accessible as possible.</p>
         <p>we encourage you to join our community and provide as much or as little information about yourself as you are comfortable with.</p>
         <p>as bootcampers ourselves, we know how stressful and intensive these courses can be, and we hope that this site allows you to make connections with people who are going through similar things and enables you to help each other as much as we have been helped by our fellows from bootcamps past and present!</p>
-        <p></p>
+        <p>we hope to see you on the social sites and thank you for taking an interest in our little project.</p>
+        <p><i>and remember: trans rights are human rights - black lives matter - solidarity forever!</i></p>
       </div>
     </div>
   )
